9ffe4ea52081d77574f9e9cb67741c04968d23e6
The Chap <zpfahl@SAVERS.COM>
2021-01-29 11:40:47 -0800

    Limit Pumps Per MILESTONE #TraqJaq - Per ISSUE #37   - create pump session view class   - return only alotted pumps to view   - maintain total pump count

---
+++ Quarantine/Models/PumpSessionsView.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Quarantine.Models
+{
+    public class PumpSessionsView
+    {
+        public List<Pump> Pumps { get; set; }
+        public int TotalPumps { get; set; }
+    }
+}
